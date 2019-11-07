20f88230aefe7a7cd7b92d3cea2f473679a552b0
mag <maguilamo.c@gmail.com>
2019-11-07 16:00:10 -0400

    update gulpfile and add new pakages

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,138 @@
-//https://gist.github.com/jeromecoupe/0b807b0c1050647eb340360902c3203a
+"use strict";
 
-const { gulp, src, dest, parallel, watch, series } = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const prefix      = require('gulp-autoprefixer');
-const cp          = require('child_process');
-const sourcemaps  = require('gulp-sourcemaps');
+// Load plugins
+const autoprefixer = require("autoprefixer");
+const browsersync = require("browser-sync").create();
+const cp = require("child_process");
+const cssnano = require("cssnano");
+const del = require("del");
+const eslint = require("gulp-eslint");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const newer = require("gulp-newer");
+const plumber = require("gulp-plumber");
+const postcss = require("gulp-postcss");
+const rename = require("gulp-rename");
+const sass = require("gulp-sass");
+const webpack = require("webpack");
+const webpackconfig = require("./webpack.config.js");
+const webpackstream = require("webpack-stream");
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
-var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
-};
-
-function jekyllBuild(){
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'});
-}
-
-function browserSyncInit(){
-  browserSync({
-      server: {
-          baseDir: '_site'
-      }
+// BrowserSync
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: "./_site/"
+    },
+    port: 3000
   });
-}
-
-function browserSyncReload(done) {
-  browserSync.reload();
   done();
 }
 
-function sassBuild(){
-  return src('_scss/main.scss')
-      .pipe(sass({
-          includePaths: ['scss'],
-          onError: browserSync.notify
-      }))
-      .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-      .pipe(dest('_site/assets/css'))
-      .pipe(browserSync.reload({stream:true}))
-      .pipe(dest('assets/css'));
+// BrowserSync Reload
+function browserSyncReload(done) {
+  browsersync.reload();
+  done();
 }
 
-exports.default = series(parallel(sassBuild, jekyllBuild), browserSyncInit );
+// Clean assets
+function clean() {
+  return del(["./_site/assets/"]);
+}
 
-watch('./_scss/**/*.scss', sassBuild);
-watch(['*.html', '*.md','_layouts/*.html' , '_posts/*', '_data/*'], series(jekyllBuild,browserSyncReload ));
+// Optimize Images
+function images() {
+  return gulp
+    .src("./assets/images/**/*")
+    .pipe(newer("./_site/assets/images"))
+    .pipe(
+      imagemin([
+        imagemin.gifsicle({ interlaced: true }),
+        imagemin.jpegtran({ progressive: true }),
+        imagemin.optipng({ optimizationLevel: 5 }),
+        imagemin.svgo({
+          plugins: [
+            {
+              removeViewBox: false,
+              collapseGroups: true
+            }
+          ]
+        })
+      ])
+    )
+    .pipe(gulp.dest("./_site/assets/images"));
+}
+
+// CSS task
+function css() {
+  return gulp
+    .src('_scss/*.scss')
+    .pipe(plumber())
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(gulp.dest("./_site/assets/css/"))
+    .pipe(browsersync.stream());
+}
+
+// Lint scripts
+function scriptsLint() {
+  return gulp
+    .src(["./assets/js/**/*", "./gulpfile.js"])
+    .pipe(plumber())
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
+}
+
+// Transpile, concatenate and minify scripts
+function scripts() {
+  return (
+    gulp
+      .src(["./assets/js/**/*"])
+      .pipe(plumber())
+      .pipe(webpackstream(webpackconfig, webpack))
+      // folder only, filename is specified in webpack config
+      .pipe(gulp.dest("./_site/assets/js/"))
+      .pipe(browsersync.stream())
+  );
+}
+
+// Jekyll
+function jekyll() {
+  return cp.spawn("bundle", ["exec", "jekyll", "build"], { stdio: "inherit" });
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch("./assets/scss/**/*", css);
+  gulp.watch("./assets/js/**/*", gulp.series(scriptsLint, scripts));
+  gulp.watch("./assets/images/**/*", images);
+  gulp.watch(
+    [
+      "./_includes/**/*",
+      "./_layouts/**/*",
+      "./_pages/**/*",
+      "./_posts/**/*",
+      "./_projects/**/*"
+    ],
+    gulp.series(jekyll, browserSyncReload)
+  );
+}
+
+
+// define complex tasks
+const js = gulp.series(scriptsLint, scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, jekyll, js));
+const watch = gulp.parallel(watchFiles, browserSync);
+
+// export tasks
+exports.images = images;
+exports.css = css;
+exports.js = js;
+exports.jekyll = jekyll;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = build;
