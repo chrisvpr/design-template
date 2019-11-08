bf02a2f793422789cda60241c64d9eddd3c4085b
mag <maguilamo.c@gmail.com>
2019-11-08 16:20:14 -0400

    Restructure project and Fix image minification process
    Use pngquant for better performance and include sweets defaults for it to
    deliver results closed to TinyPNG.
    
    Optimized images will reside in _site/assets/images from now on. Better
    quality images will be in the development side of the project, inside
    ./assets/images (no action has to be taken here).

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,37 @@
 "use strict";
 
-// Load plugins
-const autoprefixer = require("autoprefixer");
 const browsersync = require("browser-sync").create();
-const cp = require("child_process");
-const cssnano = require("cssnano");
-const del = require("del");
-const eslint = require("gulp-eslint");
-const gulp = require("gulp");
-const imagemin = require("gulp-imagemin");
-const newer = require("gulp-newer");
-const plumber = require("gulp-plumber");
-const postcss = require("gulp-postcss");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const webpack = require("webpack");
-const webpackconfig = require("./webpack.config.js");
-const webpackstream = require("webpack-stream");
 
-// BrowserSync
+const gulp   = require("gulp");
+
+const cp     = require("child_process");
+const del    = require("del");
+const eslint = require("gulp-eslint");
+
+const newer    = require("gulp-newer");
+const imagemin = require("gulp-imagemin");
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+
+
+const sass         = require("gulp-sass");
+const rename       = require("gulp-rename");
+const plumber      = require("gulp-plumber");
+const postcss      = require("gulp-postcss");
+const cssnano      = require("cssnano");
+const autoprefixer = require("autoprefixer");
+
+const webpack       = require("webpack");
+const webpackstream = require("webpack-stream");
+const webpackconfig = require("./webpack.config.js");
+
 function browserSync(done) {
   browsersync.init({
-    server: {
-      baseDir: "./_site/"
-    },
-    port: 3000
+    server: {baseDir: "./_site/"}
   });
   done();
 }
 
-// BrowserSync Reload
 function browserSyncReload(done) {
   browsersync.reload();
   done();
@@ -37,8 +39,12 @@ function browserSyncReload(done) {
 
 // Clean assets
 function clean() {
-  return del(["./_site/assets/"]);
+  return del(["./_site/assets/css", "./_site/assets/fonts"]);
 }
+
+/*
+{quality: '70-90', speed: 1, floyd: 1}
+*/
 
 // Optimize Images
 function images() {
@@ -48,8 +54,8 @@ function images() {
     .pipe(
       imagemin([
         imagemin.gifsicle({ interlaced: true }),
-        imagemin.jpegtran({ progressive: true }),
-        imagemin.optipng({ optimizationLevel: 5 }),
+        mozjpeg({quality: 80}),
+        pngquant({quality: [0.7,0.90], speed: 1, floyd: 1}),
         imagemin.svgo({
           plugins: [
             {
@@ -66,10 +72,9 @@ function images() {
 // CSS task
 function css() {
   return gulp
-    .src('_scss/*.scss')
+    .src("./assets/scss/**/*.scss")
     .pipe(plumber())
     .pipe(sass({ outputStyle: "expanded" }))
-    .pipe(gulp.dest("./_site/assets/css/"))
     .pipe(rename({ suffix: ".min" }))
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(gulp.dest("./_site/assets/css/"))
@@ -93,8 +98,7 @@ function scripts() {
       .src(["./assets/js/**/*"])
       .pipe(plumber())
       .pipe(webpackstream(webpackconfig, webpack))
-      // folder only, filename is specified in webpack config
-      .pipe(gulp.dest("./_site/assets/js/"))
+      .pipe(gulp.dest("./_site/assets/js/"))  // folders only
       .pipe(browsersync.stream())
   );
 }
@@ -109,25 +113,18 @@ function watchFiles() {
   gulp.watch("./assets/scss/**/*", css);
   gulp.watch("./assets/js/**/*", gulp.series(scriptsLint, scripts));
   gulp.watch("./assets/images/**/*", images);
-  gulp.watch(
-    [
-      "./_includes/**/*",
-      "./_layouts/**/*",
-      "./_pages/**/*",
-      "./_posts/**/*",
-      "./_projects/**/*"
-    ],
-    gulp.series(jekyll, browserSyncReload)
-  );
+  gulp.watch(["./_includes/**/*","./_layouts/**/*","./_pages/**/*"], gulp.series(jekyll, browserSyncReload));
 }
 
-
-// define complex tasks
-const js = gulp.series(scriptsLint, scripts);
-const build = gulp.series(clean, gulp.parallel(css, images, jekyll, js));
+// Tasks that will run in parallel
 const watch = gulp.parallel(watchFiles, browserSync);
 
-// export tasks
+// Task that will run in serie
+const js    = gulp.series(scriptsLint, scripts);
+const build = gulp.series(clean, gulp.parallel(css, images, jekyll, js));
+const dev   = gulp.series(build, watch);
+
+// export tasks; This will make tasks visible in the command line as well
 exports.images = images;
 exports.css = css;
 exports.js = js;
@@ -135,4 +132,4 @@ exports.jekyll = jekyll;
 exports.clean = clean;
 exports.build = build;
 exports.watch = watch;
-exports.default = build;
+exports.default = dev;
